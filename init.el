e32731f5fd90dcd215704f07a4dab5bffcb9a279
Alex Vorobiev <alexander.vorobiev@gmail.com>
2013-05-06 14:53:55 -0500

    Remove leftover packages on start up

--- init.el
+++ init.el
@@ -9,10 +9,30 @@
 (setq starter-kit-dir
       (file-name-directory (or load-file-name (buffer-file-name))))
 
-;; org is special
-;; remove org-mode shipped with emacs from the load-path
 (require 'cl)
 
+;; Remove leftover older packages
+(let* ((all-dirs
+        (remove-if-not
+         #'(lambda (f) (and (file-directory-p f) (string-match "-" f)))
+         (directory-files "~/.emacs.d/elpa" t)))
+       (unique-dirs 
+        (cl-remove-duplicates
+         all-dirs
+         :test #'(lambda (s1 s2)
+                   (flet ((strip-off-version
+                           (str)
+                           (substring str 0
+                                      (1- (string-match "[0-9]+\.[0-9]+" str)))))
+                     (string= (strip-off-version s1)
+                              (strip-off-version s2))))))
+       (to-delete (cl-set-difference all-dirs unique-dirs)))
+  (message "Deleting older packages:\n %s"
+           (mapconcat #'(lambda (d) (delete-directory d t) d) to-delete "...ok\n")))
+
+
+;; org is special
+;; remove org-mode shipped with emacs from the load-path
 (setq custom-org-path (car (file-expand-wildcards
                             (concat starter-kit-dir "elpa/org-plus-contrib-20*"))))
 (when custom-org-path 
@@ -29,4 +49,4 @@
 (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))
 
 ;;; init.el ends here
-(put 'narrow-to-region 'disabled nil)
+
